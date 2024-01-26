b1801ad848a02b5e4add623686cae49d1536b62f
realismail04 <151588136+realismail04@users.noreply.github.com>
2024-01-26 21:04:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+function guessNumber() {
+  // generating a random integer from 1 to 100
+  const random = Math.floor(Math.random() * 100) + 1;
+
+  // set the maximum number of chances
+  const maxChances = 10;
+
+  // take input from the user
+  let number = parseInt(prompt("Guess a number from 1 to 100: "));
+
+  let attempts = 1; // track the number of attempts
+
+  // take the input until the guess is correct or maximum chances are reached
+  while (number !== random && attempts < maxChances) {
+    // Check if the input is a valid number
+    if (isNaN(number) || number < 1 || number > 100) {
+      alert("Wrong! Please enter a valid number between 1 and 100.");
+    } else {
+      // Provide a hint if the guess is incorrect
+      if (number < random) {
+        alert("Wrong! Try again. The correct number is greater.");
+      } else {
+        alert("Wrong! Try again. The correct number is less.");
+      }
+    }
+    // take input again
+    number = parseInt(prompt("Guess a number from 1 to 100: "));
+    attempts++;
+  }
+
+  // check if the guess is correct or maximum chances are reached
+  if (number == random) {
+    alert(
+      `Congratulations! You guessed the correct number in ${attempts} attempts.`,
+    );
+  } else {
+    alert(
+      `Sorry, you've reached the maximum number of attempts. The correct number was ${random}.`,
+    );
+  }
+}
+
+guessNumber();
